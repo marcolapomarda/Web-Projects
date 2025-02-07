6aeed7b7fbe62176c4f0b97c26695745bb5a6d56
Marco Lapomarda <marco.lapomarda98@gmail.com>
2025-02-07 13:04:15 +0100

    change <a> to <Link> in Navbar component - css changes

--- React/lapo-blog/src/Navbar.js
+++ React/lapo-blog/src/Navbar.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Lapo's Muay Thai Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" id="create-blog">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" id="create-blog">New Blog</Link>
             </div>
         </nav>
-     );
+    );
 }
  
 export default Navbar;
